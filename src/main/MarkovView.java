451e13d21e79dfbff1018839a2e260cedebf9f5c
rafael146 <crafael.araujo@gmail.com>
2016-02-01 21:11:22 -0300

    gpl

--- src/main/MarkovView.java
+++ src/main/MarkovView.java
@@ -1,6 +1,25 @@
 package main;
 
 
+/*
+This file is part of MarkovEditor.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
 import org.jhotdraw.app.action.edit.RedoAction;
 import org.jhotdraw.app.action.edit.UndoAction;
 import org.jhotdraw.draw.io.OutputFormat;
